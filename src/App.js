60d9f2731739cbfba4c3774dcad5b763e0d1ca96
efekanefos <efekanefos@gmail.com>
2021-02-11 19:36:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PopularMovies from "./components/PopularMovies";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Efekan</h1>
+      <PopularMovies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PopularMovies.jsx
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react";
+
+export default function PopularMovies(){
+    const [movies, setMovies] = useState()
+    useEffect(() => {
+        getPopular();
+    }, [])
+
+    function getPopular(){
+        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=ed31b81064e7c617fa201d727de6c48c&language=en-US&page=1").then(response => response.json()).then(json => setMovies(json)).catch(err => console.log(err));
+        console.log("calisti")
+    }
+
+    return(
+        <div>
+            <h1>hello</h1>
+            {movies[0]}
+        </div>
+    )
+
+
+
+}
